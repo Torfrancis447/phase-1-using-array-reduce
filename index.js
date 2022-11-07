97eb719922f4c2f50b4e09a87f9a76a48cc560b3
tor <torfrancis447@gmail.com>
2022-11-06 21:35:04 -0500

    commit tree 926b3a50fc298c528f6e272b520a481ef06bee42

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function totalBatteries (batteries){
+    let totalBattery = 0;
+
+    for(const battery of batteries) {
+        totalBattery += battery;
+    }
+    return totalBattery
+}
+console.log(totalBatteries(batteryBatches))
+
+
